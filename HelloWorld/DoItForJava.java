02409c0342f1c541e3fe66d51b21cd992c9e166d
M0nsoon <muelleimer9009@gmail.com>
2019-05-11 02:20:42 +0200

    added java & scala

--- HelloWorld/DoItForJava.java
+++ HelloWorld/DoItForJava.java
@@ -1,8 +1,13 @@
 package HelloWorld;
 
+import java.util.Scanner;
+
 public class DoItForJava {
     static void printOut() {
-        System.out.println("Hello World");
+        System.out.println("Who are you?");
+        Scanner sc = new Scanner(System.in);
+        String name = sc.nextLine();
+        System.out.println("Hello " + name);
     }
 
     public static void main(String[] args) {
--- HelloWorld/Hello.scala
+++ HelloWorld/Hello.scala
@@ -1,5 +1,12 @@
 package HelloWorld
 
+
+
 class Hello {
-    def doTheMagic:Unit ={ print("Hello World")}
+    def doTheMagic:Unit ={
+      print("What's your name?")
+
+      val a = readLine()
+      print("Hello " + a)
+    }
 }
